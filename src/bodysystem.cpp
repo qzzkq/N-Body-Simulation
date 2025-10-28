922efcee2af826965476bead4bff8c48574321fb
Anisimov Vladimir <v.anisimov1@g.nsu.ru>
2025-10-28 20:00:47 +0700

    Corrects

--- src/bodysystem.cpp
+++ src/bodysystem.cpp
@@ -10,7 +10,7 @@ BodySystem::BodySystem(std::vector<Object>& objects) : bodies_(objects) {
     double tMass = 0.;
     glm::dvec3 impulse = glm::dvec3(0., 0., 0.);  
 
-    if (this->bodiesAmount) {
+    if (this->bodiesAmount == 0) {
         this->systemVel = impulse; 
         this->totalMass = 0; 
     }
@@ -26,6 +26,19 @@ BodySystem::BodySystem(std::vector<Object>& objects) : bodies_(objects) {
 }
 
 glm::dvec3 BodySystem::getVel() {
+
+    glm::dvec3 impulse = glm::dvec3(0., 0., 0.); 
+
+    if (this->bodiesAmount == 0) {
+        return  glm::dvec3(0., 0., 0.); 
+    }
+    
+    for (size_t i = 0; i < this->bodiesAmount; ++i) {
+        impulse = this->bodies_.at(i).mass * this->bodies_.at(i).velocity; 
+    }
+
+    this->systemVel = impulse / this->totalMass; 
+
     return this->systemVel;
 } 
 double BodySystem::getMass() {
