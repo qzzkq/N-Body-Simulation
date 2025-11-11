5c246c3d324bbdb5f6ec2480692c816af3f55d68
Anisimov Vladimir <v.anisimov1@g.nsu.ru>
2025-11-12 01:49:18 +0700

    My task

--- src/bodysystem.cpp
+++ src/bodysystem.cpp
@@ -3,48 +3,63 @@
 #include "object.hpp" 
 #include "bodysystem.hpp"
 
-BodySystem::BodySystem(std::vector<Object>& objects) : bodies_(objects) {
-
-    this->bodies_ = objects; 
-    this->bodiesAmount = objects.size();
-    double tMass = 0.;
-    glm::dvec3 impulse = glm::dvec3(0., 0., 0.);  
-
-    if (this->bodiesAmount == 0) {
-        this->systemVel = impulse; 
-        this->totalMass = 0; 
-    }
-    
-    for (size_t i = 0; i < this->bodiesAmount; ++i) {
-        tMass += this->bodies_.at(i).mass;
-        impulse += this->bodies_.at(i).mass * this->bodies_.at(i).velocity; 
-    }
-
-    this->systemVel = impulse / tMass; 
-    this->totalMass = tMass; 
-
+BodySystem::BodySystem(std::vector<Object>& objects) {
+    calcParams(objects); 
 }
 
-glm::dvec3 BodySystem::getVel() {
-
-    glm::dvec3 impulse = glm::dvec3(0., 0., 0.); 
-
-    if (this->bodiesAmount == 0) {
-        return  glm::dvec3(0., 0., 0.); 
-    }
-    
-    for (size_t i = 0; i < this->bodiesAmount; ++i) {
-        impulse += this->bodies_.at(i).mass * this->bodies_.at(i).velocity; 
-    }
-
-    this->systemVel = impulse / this->totalMass; 
-
-    return this->systemVel;
-} 
 double BodySystem::getMass() {
     return this->totalMass; 
 } 
+
 std::size_t BodySystem::getBodiesAmount() {
     return this->bodiesAmount; 
+}
+
+glm::dvec3 BodySystem::getVel() {
+    return this->systemVel; 
+}
+
+glm::dvec3 BodySystem::getCenter() {
+    return this->massCenter; 
+} 
+
+void BodySystem::calcParams(std::vector<Object>& objects) {
+
+    glm::dvec3 impulse = glm::dvec3(0., 0., 0.); 
+    glm::dvec3 center = glm::dvec3(0., 0., 0.);
+    glm::dvec3 massMoment = glm::dvec3(0., 0., 0.);
+    double tMass = 0.; 
+    this->bodiesAmount = objects.size(); 
+
+    if (this->bodiesAmount == 0) {
+        this->systemVel = impulse; 
+        this->totalMass = 0;
+        this->massCenter = center; 
+        return; 
+    }
+    
+    for (size_t i = 0; i < this->bodiesAmount; ++i) {
+        tMass += objects.at(i).mass; // вычисляем массу системы 
+        impulse += objects.at(i).mass * objects.at(i).velocity; // вычисляем импульс 
+        massMoment += objects.at(i).mass * objects.at(i).position; // сумма произведений массы на координаты 
+
+    }
+
+    // считаем центр масс 
+    center = massMoment / tMass; 
+
+    // записываем параметры 
+    this->massCenter = center; 
+    this->totalMass = tMass; 
+    this->systemVel = impulse / this->totalMass; 
+
+}
+
+void BodySystem::transPointToSystem(std::vector<Object>& objects) {
+
+    for (size_t i = 0; i < this->bodiesAmount; ++i) {
+        objects.at(i).velocity -= this->systemVel; 
+    }
+
 }
 
