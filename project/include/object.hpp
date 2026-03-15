7b3c83c6f511ec9f5257830f727ddeeea52b87e1
Ivan <mister.vanya1234@gmail.com>
2026-03-15 17:46:43 +0700

    better tracers

--- project/include/object.hpp
+++ project/include/object.hpp
@@ -7,51 +7,49 @@
 
 class Object {
 public:
-    // Физические параметры
     glm::dvec3 position;
     glm::dvec3 velocity;
     double mass;
     double density;
     
-    // Визуальные параметры
     float radius; 
     glm::vec4 color;
     std::string name;
-    std::deque<glm::vec3> trail;
+    std::vector<glm::vec3> trail;
 
-    // Флаги логики
     bool Initalizing = false;
     bool Launched = false;
     bool target = false;
 
-    // Конструктор
     Object(glm::dvec3 initPosition, glm::dvec3 initVelocity,
            std::optional<double> massOpt,
            std::optional<double> densityOpt,
            std::optional<double> radiusOpt);
 
-    // Методы физики
     void UpdatePos(double deltaTime);
     void accelerate(double x, double y, double z, double deltaTime);
     glm::dvec3 GetPos() const;
 
-    void updateTrail() {
-        trailSkipCounter++;
-        if (trailSkipCounter > 5) { 
+    void updateTrail(bool force = false) {
+        if (force || trail.empty()) { 
             trail.push_back(position);
-            if (trail.size() > MAX_TRAIL_LENGTH) {
-                trail.pop_front();
-            }
-            trailSkipCounter = 0;
+            return;
+        }
+
+        float distanceThreshold = 1.0f;
+        if (glm::distance(glm::vec3(position), trail.back()) > distanceThreshold) {
+            trail.push_back(position);
+        }
+
+        if (trail.size() > MAX_TRAIL_LENGTH) {
+            trail.erase(trail.begin());
         }
     }
 
     void resetTrail() {
         trail.clear();
-        trailSkipCounter = 0;
     }
 
 private: 
     static const size_t MAX_TRAIL_LENGTH = 150; 
-    int trailSkipCounter = 0;
 };
